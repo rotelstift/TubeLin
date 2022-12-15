07d203bd953b0cfd7a9053e7781ee847b8a3a2b9
rotelstift <rotelstift@gmail.com>
2022-12-15 14:38:22 +0900

    例題bot完成

---
+++ example_bot.py
@@ -0,0 +1,26 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import json
+
+setting_file = open('settings.json', 'r')
+setting = json.load(setting_file)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(setting["bot_token"])
